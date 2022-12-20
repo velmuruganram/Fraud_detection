71e81e76869b5202abee89ae2105d2ebe1b9249d
velmuruganram <velmurugan.chettiar@gmail.com>
2022-12-20 22:18:50 +0530

    model2 is the final model

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,13 @@
 
 print('adding all ml code here')
+
+def model1():
+    print('Adding code for mlmodel1')
+
+def model2():
+    print('Adding code for mlmodel2')
+
+def model3():
+    print('Adding code for mlmodel3')
+
+print('model2 is performing well')
